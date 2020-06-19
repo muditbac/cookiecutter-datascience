0037883af79338cea4b79f40c85a5bdd7d4b63be
muditbac-curefit <mudit5bachhawat@gmail.com>
2020-06-19 08:10:51 +0530

    sacred and mongo fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils.py
@@ -1,16 +1,21 @@
 import sacred
+from sacred import SETTINGS
 from sacred.observers import MongoObserver, FileStorageObserver
+
+SETTINGS.DISCOVER_SOURCES = 'dir'
 
 
 def get_sacred_experiment(name, observer='mongo'):
-	ex = sacred.Experiment(name)
-	if observer == 'mongo':
-		ex.observers.append(MongoObserver())
-	else:
-		ex.observers.append(FileStorageObserver('data/sacred/'))
+    ex = sacred.Experiment(name)
+    if observer == 'mongo':
+        ex.observers.append(MongoObserver(url='mongodb://{{cookiecutter.mongo_user}}:'
+                                              '{[cookiecutter.mongo_password}}@127.0.0.1:27017',
+                                          db_name='sacred'))
+    else:
+        ex.observers.append(FileStorageObserver('data/sacred/'))
 
-	return ex
+    return ex
 
 
 def quick_parse(*args):
-	pass
+    pass
